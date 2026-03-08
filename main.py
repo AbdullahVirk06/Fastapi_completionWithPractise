b5fee1ea552580b6169d5d2d2b857afe6e4dc512
Muhammad Abdullah <whello544@gmail.com>
2026-03-09 01:44:20 +0500

    Implement delete and update endpoints for posts
    Added functionality to delete and update posts by ID.

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ def find_post(id):
         if p['id'] == id:
             return p
 
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
 my_posts = [{"title" : "title of post 1", "content" : " content of post 1","id": 1},{"title":"favourite food","content":"I like pizza","id":2}]
 @app.get("/")
 def root():
@@ -38,7 +43,28 @@ def create_post(post : Post):
 @app.get("/post/{id}")
 def get_post(id : int, response : Response):
     post = find_post(id)
+
+
     if not post:
         response.status_code = status.HTTP_404_NOT_FOUND
         return {"message": f"post with id: {id} was not found"}
+
     return{"post_detail": post}
+
+@app.delete("/post/{id}",status_code = status.HTTP_204_NO_CONTENT)
+def delete_post(id : int):
+    for index, post in enumerate(my_posts):
+        if post['id'] == id:
+            my_posts.pop(index)
+            return{"message": f"post with id: {id} was successfully deleted"}
+    raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"post with id: {id} was not found")
+
+@app.put("/post/{id}")
+def update_post(id : int, post : Post):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"post with id: {id} was not found")
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return{"data": post_dict}
